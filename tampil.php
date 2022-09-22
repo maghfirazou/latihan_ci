5c208c2c84b670e1d6ac12201f88e245899313d6
Moh Fahmi Muldan Nugraha <fmagfira07@gmail.com>
2022-09-22 12:13:14 +0700

    first commit

---
+++ tampil.php
@@ -0,0 +1,10 @@
+<html>
+<head>   
+<title>Latihan Pertama</title>
+<head>
+<body>
+    <p>Latihan Pertama saya belajar codeigniter</p>
+    <p>Framework Codeigniter sangat mudah dipelajari</p>
+
+    </body>
+    </html>
